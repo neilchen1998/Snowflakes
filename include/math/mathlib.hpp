e82046a44fe44e75c118b5094d20b01ae07d0d0b
Neil Chen <neilchen1998@gmail.com>
2024-01-25 19:32:26 -0600

    added boost_normal_distribution func

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -1,4 +1,10 @@
 #ifndef INCLUDE_MATH_MATHLIB_H_
 #define INCLUDE_MATH_MATHLIB_H_
 
+/// @brief 
+/// @param mean the mean of the distribution (μ)
+/// @param sd the standard deviation (σ)
+/// @return 
+double boost_normal_distribution(double mean = 0.0, double sd = 1.0);
+
 #endif  // INCLUDE_MATH_MATHLIB_H_
