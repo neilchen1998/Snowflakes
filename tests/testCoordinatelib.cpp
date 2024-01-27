d0f74c130d616a75e197a4273ec90b7cbacfa0f1
Neil Chen <neilchen1998@gmail.com>
2024-01-26 21:10:59 -0600

    created Magnitude test case

--- tests/testCoordinatelib.cpp
+++ tests/testCoordinatelib.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN
 
-#include <cmath>   // round
+#include <cmath>   // round, sqrt
 #include <catch2/catch.hpp>
 
 #include "coordinate/coordinatelib.hpp"
@@ -14,19 +14,29 @@ TEST_CASE( "Coordinate", "Point" )
         REQUIRE (p0.y == Approx(0));
     }
 
-    constexpr double x = 1.2, y = 2.4;
-    Point p1(1.2, 2.4);
+    constexpr double x1 = 1.2, y1 = 2.4;
+    Point p1(x1, y1);
 
     SECTION("Can Create Using the Default Constructor with Custom Values")
     {
-        REQUIRE (p1.x == Approx(x));
-        REQUIRE (p1.y == Approx(y));
+        REQUIRE (p1.x == Approx(x1));
+        REQUIRE (p1.y == Approx(y1));
     }
 
-    const double expectedD = sqrt(x*2 + y*2);
+    constexpr double x2 = 2.59, y2 = 21.4;
+    Point p2(x2, y2);
+
+    const double expectedD = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
     SECTION("Distance between Two Points")
     {
-        auto d = p1.Distance(p0);
+        auto d = p1.Distance(p2);
         REQUIRE (d == Approx(expectedD));
+    }
+
+    const double expectedM = sqrt(x1*2 + y1*2);
+    SECTION("Magnitude of a Point")
+    {
+        auto d = p1.Magnitude();
+        REQUIRE (d == Approx(expectedM));
     }
 }
