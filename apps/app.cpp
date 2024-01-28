4ab29cc0677aa89d658d95fd38c6ba5fd7d1d68a
Neil Chen <neilchen1998@gmail.com>
2024-01-28 14:34:53 -0600

    made main func generate snowflakes and save images

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
+#include <string>
 
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
@@ -20,38 +21,48 @@
 
 int main()
 {   
-    // create a black canvas
-    cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
-
-    // generates particles
-    constexpr size_t NUM_CIRCLES = 50;
-    std::vector<Circle> circles(NUM_CIRCLES * 12);
-    circles[0].c = Vector(0, 0);
-    for (size_t i = 1; i < NUM_CIRCLES; i++)
+    bool canSave = true;
+    constexpr unsigned char NUM_RENDERS = 10;
+    // renders N images
+    for (unsigned char render = 0; render < NUM_RENDERS; render++)
     {
-        Vector start = circles[i - 1].c;
-        int radiusStart = circles[i - 1].radius;
-        Vector dir = Vector(1, boost_normal_distribution(1));
-        int radiusNew = boost_uniform_int_distribution(10);
-        Vector cNew = GenerateNextCircle(start, radiusStart, dir, radiusNew);
-        circles[i].c = cNew;
-        circles[i].radius = radiusNew;
-    }
-
-    for (size_t spin = 1; spin < 12; spin++)
-    {
-        for (size_t i = 0; i < NUM_CIRCLES; i++)
+        // creates a black canvas
+        cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
+        constexpr size_t NUM_CIRCLES = 50;
+        std::vector<Circle> circles(NUM_CIRCLES * 12);
+        circles[0].c = Vector(0, 0);
+        
+        // generates particles
+        for (size_t i = 1; i < NUM_CIRCLES; i++)
         {
-            circles[spin * NUM_CIRCLES + i].c = Vector::Rotate(circles[i].c, DEG_TO_RAD(30*spin));
-            circles[spin * NUM_CIRCLES + i].radius = circles[i].radius;
+            Vector start = circles[i - 1].c;
+            int radiusStart = circles[i - 1].radius;
+            Vector dir = Vector(1, boost_normal_distribution(1));
+            int radiusNew = boost_uniform_int_distribution(10);
+            Vector cNew = GenerateNextCircle(start, radiusStart, dir, radiusNew);
+            circles[i].c = cNew;
+            circles[i].radius = radiusNew;
         }
+
+        for (size_t spin = 1; spin < 12; spin++)
+        {
+            for (size_t i = 0; i < NUM_CIRCLES; i++)
+            {
+                circles[spin * NUM_CIRCLES + i].c = Vector::Rotate(circles[i].c, DEG_TO_RAD(30*spin));
+                circles[spin * NUM_CIRCLES + i].radius = circles[i].radius;
+            }
+        }
+
+        // draws all circles on the canvas
+        DrawAllCircles(img, circles);
+
+        // save image
+        std::string filepath("outputs/");
+        std::string filename = "image-" + std::to_string(render) + ".jpg";
+        canSave = SaveImages(filepath + filename, img);
+        if (!canSave)
+            return EXIT_FAILURE;
     }
-    
-
-    // draws all circles on the canvas
-    DrawAllCircles(img, circles);
-
-    DisplayImage("Image", img);
 
     return EXIT_SUCCESS;
 }
