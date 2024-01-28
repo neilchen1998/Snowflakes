86702e6d9928e674711c3c8a05649bdabdbfaa48
Neil Chen <neilchen1998@gmail.com>
2024-01-28 13:29:36 -0600

    added DrawAllCircles func

--- include/graph/graphlib.hpp
+++ include/graph/graphlib.hpp
@@ -2,9 +2,31 @@
 #define INCLUDE_GRAPH_GRAPHLIB_H_
 
 #include <string>
+#include <vector>
 
 #include <opencv2/core/base.hpp>
+#include "coordinate/vectorlib.hpp"
 
-void DisplayImage(const std::string& windowName, cv::Mat img);
+struct Circle
+{
+    Vector c;
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned int radius;
+
+    Circle() : c(0.0, 0.0), r(255), g(255), b(255), radius(0) {}
+    Circle(Vector c, unsigned char r, unsigned char g, unsigned char b, unsigned int radius) : c(c), r(r), g(g), b(b), radius(radius) {}
+};
+
+/// @brief Display the image using OpenCV
+/// @param windowName the name of the window
+/// @param img the image (OpenCV format)
+void DisplayImage(const std::string& windowName, cv::Mat& img);
+
+/// @brief Draw all circles in the vector container on the canvas
+/// @param img the canvas
+/// @param circles a vector of circles
+void DrawAllCircles(cv::Mat& img, const std::vector<Circle>& circles);
 
 #endif  // INCLUDE_GRAPH_GRAPHLIB_H_
--- src/graphlib.cpp
+++ src/graphlib.cpp
@@ -2,10 +2,28 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
-void DisplayImage(const std::string& windowName, cv::Mat img)
+#include "graph/graphlib.hpp"
+#include "coordinate/vectorlib.hpp"
+
+#define FILLED -1
+#define ROWS 1024
+#define COLS 1024
+#define CENTER (ROWS/2)
+
+void DisplayImage(const std::string& windowName, cv::Mat& img)
 {
     cv::imshow(windowName, img);
     cv::waitKey(0);
 
     return;
+}
+
+void DrawAllCircles(cv::Mat& img, const std::vector<Circle>& circles)
+{
+    auto itr = circles.cbegin();
+    while (itr != circles.cend())
+    {
+        cv::circle(img, cv::Point(itr->c.x + CENTER, itr->c.y + CENTER), itr->radius, CV_RGB(itr->r, itr->g, itr->b), FILLED);
+        ++itr;
+    }
 }
