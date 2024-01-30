03abedf1e1ab1f2df1a73b889ecbd488840f653c
Neil Chen <neilchen1998@gmail.com>
2024-01-29 22:06:13 -0600

    updated the main code

--- apps/app.cpp
+++ apps/app.cpp
@@ -24,7 +24,7 @@
 int main()
 {   
     bool canSave = true;
-    constexpr unsigned char NUM_RENDERS = (DEBUG_MODE) ? 5 : 10;
+    constexpr unsigned char NUM_RENDERS = (DEBUG_MODE) ? 1 : 10;
     // renders N images
     for (unsigned char render = 0; render < NUM_RENDERS; render++)
     {
@@ -32,8 +32,7 @@ int main()
         cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
         constexpr size_t NUM_CIRCLES = 50;
         constexpr unsigned char NUM_SPINS = 12;
-        constexpr unsigned char THETA = 360 / NUM_SPINS;
-        std::vector<Circle> circles(NUM_CIRCLES * NUM_SPINS);
+        std::vector<Circle> circles(NUM_CIRCLES * 2 * NUM_SPINS);
         circles[0].c = Vector(0, 0);
         
         // generates particles
@@ -41,24 +40,16 @@ int main()
         {
             Vector start = circles[i - 1].c;
             int radiusStart = circles[i - 1].radius;
-            Vector dir = Vector(1, boost_normal_distribution(1));
+            Vector dir = Vector(1, boost_normal_distribution(1, 0.3));
             int radiusNew = boost_uniform_int_distribution(7);
             Vector cNew = GenerateNextCircle(start, radiusStart, dir, radiusNew);
             circles[i].c = cNew;
             circles[i].radius = radiusNew;
         }
 
-        for (size_t spin = 1; spin < NUM_SPINS; spin++)
-        {
-            for (size_t i = 0; i < NUM_CIRCLES; i++)
-            {
-                circles[spin * NUM_CIRCLES + i].c = Vector::Rotate(circles[i].c, DEG_TO_RAD(THETA*spin));
-                circles[spin * NUM_CIRCLES + i].radius = circles[i].radius;
-            }
-        }
-
         // draws all circles on the canvas
-        DrawAllCircles(img, circles);
+        // RenderImage(img, circles, NUM_SPINS);
+        RenderImage(img, circles, NUM_SPINS);
 
         #if DEBUG_MODE
 
