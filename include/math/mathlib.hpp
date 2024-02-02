357132b76af48f088c2dbc04a0e5e585e9b3cc53
Neil Chen <neilchen1998@gmail.com>
2024-02-01 21:10:20 -0600

    added func brief

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -1,12 +1,16 @@
 #ifndef INCLUDE_MATH_MATHLIB_H_
 #define INCLUDE_MATH_MATHLIB_H_
 
-/// @brief 
+/// @brief Generates a double from the normal distribution
 /// @param mean the mean of the distribution (μ)
 /// @param sd the standard deviation (σ)
-/// @return 
+/// @return a random double
 double boost_normal_distribution(double mean = 0.0, double sd = 1.0);
 
-int boost_uniform_int_distribution(int high = 10, int low = 1);
+/// @brief Generates an integer between min and max
+/// @param max the max value (inclusive)
+/// @param min the min value (inclusive)
+/// @return a random integer
+int boost_uniform_int_distribution(int max = 10, int min = 1);
 
 #endif  // INCLUDE_MATH_MATHLIB_H_
