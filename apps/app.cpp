8c5b55d79fb86ebd4164582a4ba13d3519c7f55b
Neil Chen <neilchen1998@gmail.com>
2024-02-01 21:11:11 -0600

    added lable for Radiating Dendrite Snowflakes

--- apps/app.cpp
+++ apps/app.cpp
@@ -11,6 +11,7 @@
 #include "graph/graphlib.hpp"
 #include "coordinate/generatorlib.hpp"
 #include "coordinate/vectorlib.hpp"
+#include "helper/fmtlib.hpp"
 
 #define ROWS 1024
 #define COLS 1024
@@ -19,53 +20,106 @@
 #define PI 3.14159265
 #define DEG_TO_RAD(deg) ((deg) * PI / 180.0 )
 
-#define DEBUG_MODE 0
+#define DEBUG_MODE 1
+
+enum snowflakeType { crystal, radiatingDendrite };
 
 int main(int argc, char* argv[])
 {   
     bool canSave = true;
     constexpr unsigned char NUM_RENDERS = (DEBUG_MODE) ? 1 : 10;
 
-    unsigned char snowflakeType = 0;
+    unsigned char selectedSnowflakeType = radiatingDendrite;
     std::string snowflakeName;
 
-    // renders N images
-    for (unsigned char render = 0; render < NUM_RENDERS; render++)
+    switch (selectedSnowflakeType)
     {
-        // creates a black canvas
-        cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
+    case crystal:
+    {
+        const int mean = 45, sd = 10;
+        int radiusHigh = 7;
+        int radiusLow = 2;
 
-        if (snowflakeType == 0x0)
+        // renders snowflakes
+        for (unsigned char render = 0; render < NUM_RENDERS; render++)
         {
-            const int mean = 45, sd = 10;
+            // creates a black canvas
+            cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
+
             int numCrystals = boost_normal_distribution(mean, sd);
-            int radiusHigh = 7;
-            int radiusLow = 2;
             const Vector mirror(boost_normal_distribution(1, 0.1), boost_normal_distribution(1, 0.1));
+
             DrawCrystalSnowflake(img, Vector(1, 1), numCrystals, radiusHigh, radiusLow, mirror);
 
             snowflakeName = "Crystal-Snowflake";
 
             std::string label = "# of crystals: " + std::to_string(numCrystals) + " mirror: " + mirror.ToString();
 
-            // DrawRadiatingDendriteSnowflake(img, Vector(1, boost_normal_distribution(1, 0.3)), 200, boost_normal_distribution(5, 1), 20, 50 , DEG_TO_RAD(boost_normal_distribution(60, 10)), boost_normal_distribution(0.8, 0.1));
+            // put parameters on the image
+            PutLabel(img, label);
+
+            #if DEBUG_MODE
+
+                DisplayImage("image", img);
+
+            #else
+                // save image
+                std::string filepath("outputs/");
+                std::string filename = snowflakeName + "_" + std::to_string(render) + ".jpg";
+                canSave = SaveImage(filepath + filename, img);
+                if (!canSave)
+                    return EXIT_FAILURE;
+            #endif
+        }
+    }
+
+        break;
+
+    case radiatingDendrite:
+    {
+        const int mean = 200, sd = 20;
+
+        // renders snowflakes
+        for (unsigned char render = 0; render < NUM_RENDERS; render++)
+        {
+            // creates a black canvas
+            cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
+
+            const Vector mirror(boost_normal_distribution(1, 0.1), boost_normal_distribution(1, 0.1));
+            const int armLength = boost_normal_distribution(mean, sd);
+            const int armWidth = boost_normal_distribution(5, 1);
+            const int nodeLength = 20;
+            const int branchLength = 50;
+            const double theta = DEG_TO_RAD(boost_normal_distribution(60, 10));
+            const double rate = boost_normal_distribution(0.8, 0.1);
+
+            DrawRadiatingDendriteSnowflake(img, mirror, armLength, armWidth, 20, 50 , theta, rate);
+
+            snowflakeName = "Radiating-Dendrite-Snowflake";
+
+            std::string label = "armLength: " + std::to_string(armLength) + " armWidth: " + std::to_string(armWidth) + " theta: " + Formatter(theta) + " rate: " + Formatter(rate);
 
             // put parameters on the image
             PutLabel(img, label);
+
+            #if DEBUG_MODE
+
+                DisplayImage("image", img);
+
+            #else
+                // save image
+                std::string filepath("outputs/");
+                std::string filename = snowflakeName + "_" + std::to_string(render) + ".jpg";
+                canSave = SaveImage(filepath + filename, img);
+                if (!canSave)
+                    return EXIT_FAILURE;
+            #endif
         }
+    }
+        break;
 
-        #if DEBUG_MODE
-
-            DisplayImage("image", img);
-
-        #else
-            // save image
-            std::string filepath("outputs/");
-            std::string filename = snowflakeName + "_" + std::to_string(render) + ".jpg";
-            canSave = SaveImage(filepath + filename, img);
-            if (!canSave)
-                return EXIT_FAILURE;
-        #endif
+    default:
+        break;
     }
 
     return EXIT_SUCCESS;
