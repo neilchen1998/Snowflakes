98a7682194414f2ea5c1f8953333e35c0698e3aa
Neil Chen <neilchen1998@gmail.com>
2024-02-01 19:00:32 -0600

    packaged main func based on users inputs

--- apps/app.cpp
+++ apps/app.cpp
@@ -19,31 +19,40 @@
 #define PI 3.14159265
 #define DEG_TO_RAD(deg) ((deg) * PI / 180.0 )
 
-#define DEBUG_MODE 1
+#define DEBUG_MODE 0
 
 int main(int argc, char* argv[])
 {   
     bool canSave = true;
     constexpr unsigned char NUM_RENDERS = (DEBUG_MODE) ? 1 : 10;
+
+    unsigned char snowflakeType = 0;
+    std::string snowflakeName;
+
     // renders N images
     for (unsigned char render = 0; render < NUM_RENDERS; render++)
     {
         // creates a black canvas
         cv::Mat img(ROWS, COLS, CV_8UC3, CV_RGB(0, 0, 0));
 
-        const int mean = 45, sd = 10;
-        int numCrystals = boost_normal_distribution(mean, sd);
-        int radiusHigh = 7;
-        int radiusLow = 2;
-        const Vector mirror(boost_normal_distribution(1, 0.1), boost_normal_distribution(1, 0.1));
-        DrawCrystalSnowflake(img, Vector(1, 1), numCrystals, radiusHigh, radiusLow, mirror);
+        if (snowflakeType == 0x0)
+        {
+            const int mean = 45, sd = 10;
+            int numCrystals = boost_normal_distribution(mean, sd);
+            int radiusHigh = 7;
+            int radiusLow = 2;
+            const Vector mirror(boost_normal_distribution(1, 0.1), boost_normal_distribution(1, 0.1));
+            DrawCrystalSnowflake(img, Vector(1, 1), numCrystals, radiusHigh, radiusLow, mirror);
 
-        std::string label = "# of crystals: " + std::to_string(numCrystals) + " mirror: " + mirror.ToString();
+            snowflakeName = "Crystal-Snowflake";
 
-        // DrawRadiatingDendriteSnowflake(img, Vector(1, boost_normal_distribution(1, 0.3)), 200, boost_normal_distribution(5, 1), 20, 50 , DEG_TO_RAD(boost_normal_distribution(60, 10)), boost_normal_distribution(0.8, 0.1));
+            std::string label = "# of crystals: " + std::to_string(numCrystals) + " mirror: " + mirror.ToString();
 
-        // put parameters on the image
-        PutLabel(img, label);
+            // DrawRadiatingDendriteSnowflake(img, Vector(1, boost_normal_distribution(1, 0.3)), 200, boost_normal_distribution(5, 1), 20, 50 , DEG_TO_RAD(boost_normal_distribution(60, 10)), boost_normal_distribution(0.8, 0.1));
+
+            // put parameters on the image
+            PutLabel(img, label);
+        }
 
         #if DEBUG_MODE
 
@@ -52,7 +61,7 @@ int main(int argc, char* argv[])
         #else
             // save image
             std::string filepath("outputs/");
-            std::string filename = "image-" + std::to_string(render) + ".jpg";
+            std::string filename = snowflakeName + "_" + std::to_string(render) + ".jpg";
             canSave = SaveImage(filepath + filename, img);
             if (!canSave)
                 return EXIT_FAILURE;
